e1874ce3d4d6f415a8cf56b57eefb7def3f13855
Anupam <9582anupamk@gmail.com>
2024-12-14 17:27:54 +0530

    removed timeout

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -93,7 +93,6 @@ const getAllProductsStructuredByCategory = async (req, res) => {
         structuredProducts["Top Rated"] = topRated;
         structuredProducts["Offers"] = topOffer;
         structuredProducts["Todays Deal"] = todaysDeal;
-        await new Promise(resolve => setTimeout(resolve, 1000));
         return res.status(200).json({
             message: "List of products structured by category",
             products: structuredProducts,
